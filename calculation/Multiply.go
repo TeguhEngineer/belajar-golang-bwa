d9e21e0f5b0cfe952eeb0515a14d48d2b59bf974
TeguhEngineer <teguhsimdig8@gmail.com>
2025-11-19 22:14:52 +0700

    day 1: Memahami package, function, import dan membuat program kalkulasi sederhana

---
+++ calculation/Multiply.go
@@ -0,0 +1,5 @@
+package calculation
+
+func Perkalian(a int, b int) int {
+	return a * b
+}
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"belajar/calculation"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Halo, belajar perkalian")
+	result := calculation.Perkalian(2, 5)
+	fmt.Println(result)
+}
